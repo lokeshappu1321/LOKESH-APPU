d41d3318aa4c6b5ab2d7d051473c8eb4bd1f5ddd
lokeshappu1321 <146064010+lokeshappu1321@users.noreply.github.com>
2024-04-04 09:29:49 +0530

    Add files via upload

---
+++ DINESH PY 17.py
@@ -0,0 +1,7 @@
+My dict = {'x':250,'y':500,'z':410}
+print ("dictionary:"my dict)
+       total 
+       #print values using get
+       for i in my dict values():
+           total=total+id(print
+                          ("in tha total sum of values :"total))
